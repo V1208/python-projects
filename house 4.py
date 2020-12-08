f2c478c2fe69172e4a16ed9435b5fb7517d463e1
vidun <74163784+V1208@users.noreply.github.com>
2020-12-08 20:56:56 +0530

    if

---
+++ house 4.py
@@ -0,0 +1,12 @@
+number=int(input("enter number"))
+value=(number%4)
+if value==0 :
+    print("sapu")
+elif value==1 :
+    print("naa")
+elif value==2 :
+    print("olu")
+elif value==3 :
+     print("nelum")
+else :
+    print("pichcha")
