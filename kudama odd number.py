82e62816b16d1bd833c7ee44380467f8792a2ecb
Vidun Thewsara <74163784+vidun1208@users.noreply.github.com>
2021-01-15 10:20:48 -0700

    home work

--- kudama odd number.py
+++ kudama odd number.py
@@ -1,9 +1,13 @@
 x=int(input("Enter the number"))
-minimum=x
+if((x%2)==0):
+    minimum=x   
 y=1
 while(y<10):
     x=int(input("Enter the number"))
-    if(minimum>x):
+    if((x%2)!=1):
         minimum=x
+        if(minimum>x):
+            minimum=x
     y=y+1
 print(minimum)
+ 
